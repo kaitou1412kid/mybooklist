581e6e2954a5a02abafa9ca8eab95610d09fa128
kaitou1412kid <kritagyashakya0@gmail.com>
2023-01-02 10:35:54 +0545

    port

--- app.js
+++ app.js
@@ -280,6 +280,6 @@ app.get("/logout", function(req, res){
 	});
 });
 
-app.listen(3000,function(){
+app.listen(process.env.PORT || 3000,function(){
     console.log("Server is running on port 3000");
 });
